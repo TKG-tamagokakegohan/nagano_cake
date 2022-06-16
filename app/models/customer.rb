325572efdd8f29cbecbe640a1a910f277387852a
EC2 Default User <ec2-user@ip-172-31-20-241.ec2.internal>
2022-06-16 16:19:21 +0000

    customerモデルバリデーション記述

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,11 +5,22 @@ class Customer < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   #配送先
+  # !!!!!!カラムとモデル名が被ってる!!!!!!
   #belongs_to :address
+
   #ログインする時に退会済み(is_active==false)のユーザーを弾くためのメソッド
   def active_for_authentication?
     #customerのis_activeがtrueならfalseを返すようにしている
     super && (self.is_active == true)
   end
+
+  #バリデーション(email,password,is_active以外)
+  validates :last_name, presence:true
+  validates :first_name, presence:true
+  validates :last_name_kata, presence:true
+  validates :first_name_kata, presence:true
+  validates :post_code, presence:true
+  validates :address, presence:true
+  validates :phone_number, presence:true
 
 end
