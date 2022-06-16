25c008d70e783f9c39739c949fb6ec6cf4fd8b60
EC2 Default User <ec2-user@ip-172-31-20-241.ec2.internal>
2022-06-16 08:10:01 +0000

    顧客WF３、ユーザ新規登録・ヘッダログインログアウトリンク作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  #顧客側、WF3、sign_upの際のデータ操作を許可
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kata, :first_name_kata, :post_code, :address, :phone_number, :email])
+  end
 end
