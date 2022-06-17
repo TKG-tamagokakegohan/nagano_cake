bfb5d1e469ecd39928ecad806e3c08a8e311ee42
yuyuooo <103011542+yuyuooo@users.noreply.github.com>
2022-06-17 06:48:56 +0000

    [Add]管理者側注文一覧 WF:11、注文詳細　WF:13のテーブル作成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,12 @@
 class Admin::OrdersController < ApplicationController
   def index
+    @orders = Order.all
   end
 
   def show
+    @order = Order.find_by(id: params[:id])
+  end
+
+  def update
   end
 end
