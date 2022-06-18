992d7229d52b02fced71338e1c84a1016a65f3fc
EC2 Default User <ec2-user@ip-172-31-20-241.ec2.internal>
2022-06-18 11:36:11 +0000

    [Add]ヘッダー仮作成<p>中央よせまだ、

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,7 +22,7 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  #顧客側、WF3、sign_upの際のデータ操作を許可
+  #顧客側、WF3、deviceコントローラが動く前に、sign_upの際のデータ操作を許可
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kata, :first_name_kata, :post_code, :address, :phone_number, :email])
   end
