7ba25a218a7b4ebc9f031a977a245133449fa4fd
EC2 Default User <ec2-user@ip-172-31-82-87.ec2.internal>
2022-06-18 07:40:56 +0000

    商品の削除と商品の個数を変更

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -29,6 +29,27 @@ class Public::CartItemsController < ApplicationController
         end
     end
 
+    def update
+       @cart_item = current_customer.cart_items.update(cart_item_params)
+       redirect_to cart_items_path
+    end
+
+
+    def destroy
+        cart_item = CartItem.find(params[:id])
+        cart_item.destroy
+        @cart_items = CartItem.all
+         redirect_to cart_items_path
+    end
+
+    def all_destroy  #カート内全て削除
+        cart_items = CartItem.all
+        cart_items.destroy_all
+        redirect_to cart_items_path
+    end
+
+
+
 private
     def cart_item_params
         params.require(:cart_item).permit(:item_id, :item_count)
