f96c4aeab42eb1e9437c0509c160cc5acfdd83c1
yuyuooo <103011542+yuyuooo@users.noreply.github.com>
2022-06-20 07:37:53 +0000

    [Add]管理者側注文履歴、注文履歴詳細画面を追加

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,10 +1,16 @@
 class Admin::OrderDetailsController < ApplicationController
   before_action :authenticate_admin!
 
-
+  def update
+    @order_detail = OrderDetail.find(params[:id])
+    @order_detail.update(order_detail_params)
+    redirect_to request.referer
+  end
 
   private
-  def oder_detail_params
-     params.require(:order_detail).permit(:price, :item_count)
-  end
+
+    def order_detail_params
+        params.require(:order_detail).permit(:making_status)
+    end
+
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -2,4 +2,7 @@ class OrderDetail < ApplicationRecord
 # 注文機能
     belongs_to :order
     belongs_to :item
+
+    enum making_status: { "着手不可": 0, "製作待ち": 1, "製作中": 2, "製作完了": 3 }
+
 end
