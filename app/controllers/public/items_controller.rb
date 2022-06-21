54cd02afd47511d312e9a0f03b82fd86d59388bb
yuyuooo <103011542+yuyuooo@users.noreply.github.com>
2022-06-21 08:16:45 +0000

    [Fix]top画面の商品画像のリンクを追加

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -8,9 +8,7 @@ class Public::ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
     @cart_item =CartItem.new
-
     @genres = Genre.all
-    @genre = Genre.find(params[:id])
   end
 
  private
