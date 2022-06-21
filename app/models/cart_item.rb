62908eb4b0458d05dfa79324af2e41f0cfe7e202
EC2 Default User <ec2-user@ip-172-31-82-87.ec2.internal>
2022-06-21 05:12:44 +0000

    ショッピングカートの三桁区切り修正

--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -4,6 +4,14 @@ class CartItem < ApplicationRecord
 # 注文機能
    # belongs_to :order
 
+  def with_tax_price
+    (price * 1.1).floor
+  end
+
+def subtotal
+    item.with_tax_price * item_count
+end
+
 
 def sum_price # 実際に作成したサイトは税金も算出していたのでメソッドを記載していました
   item.with_tax_price*item_count
